74119b9c2a7f3829798dd80c011952e567ed7591
Jegan <83541158+jegansoftware@users.noreply.github.com>
2021-06-22 10:21:14 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+//file name must be named as "SumofAddNumber.java".
+package Mark;
+
+public class SumofAddNumber {
+
+	public static void main(String[] args) {
+		
+		SumofOddNumber oddcal=new SumofOddNumber();
+		oddcal.read();
+		oddcal.calculate();
+		oddcal.display();
+	}
+
+}
